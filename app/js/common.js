50365c6ad4b292696fc9840e19191e9f8a67115e
Artyom Rudavin <arrudavin7@gmail.com>
2019-02-18 18:10:36 +0200

    nine

--- app/js/common.js
+++ app/js/common.js
@@ -19,6 +19,71 @@ $(function() {
 		}
 	}
 
-	console.log(window.pageYOffset)
+});
 
+// Header menu
+
+	$(function() {
+
+		$('#icon').on('click', function(e) {
+			e.preventDefault();
+
+			// $('.icon').toggleClass('active');
+
+			var menu = $('#menu');
+
+			menu.slideToggle();
+		});
+	});
+
+// Scroll to
+
+$(function() {
+
+	$('.menu-f a, #menu a').click( function(){ // ловим клик по ссылке с классом go_to
+	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top - 75}, 2000); // анимируем скроолинг к элементу scroll_el
+
+	    if ( $(window).width() < 768 ) {
+	    	// console.log(this);
+	    	// $('.icon').toggleClass('active');
+	    	var menu = $('#menu');
+	    	menu.slideToggle();
+	    };	    
+	}
+	    return false; // выключаем стандартное действие
+	});
+
+});
+
+//E-mail Ajax Send
+
+$(function() {
+
+	$("#headerForm, #headerFormMain, #mainForm, #akciyaForm1, #akciyaForm2, #deliveryForm").submit(function() { //Change
+		var th = $(this);
+
+		$.ajax({
+			type: "POST",
+			url: "rest.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			// alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 500);
+			location.href = "https://avtoclav.bondareff.com.ua/sps/";
+		});
+		return false;
+	});
+
+
+});
+
+// PhoneMask
+
+$(function() {
+	$('[type="tel"]').mask('+389999999999');
 });
